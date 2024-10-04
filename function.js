526178ca65969653a7d35b9b4e8856188ae61265
Sharick Huasco <sharick.huasco.pure@vallegrande.edu.pe>
2024-10-04 16:38:31 -0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,17 @@
+
+
+
+var loader = document.getElementById("loader");
+
+window.addEventListener("load", function () {
+    setTimeout(function () {
+        loader.style.display = "none";
+    }, 1500);
+});
+
+function nuestraOds() {
+    location.href = "NuestraOds.html"
+}
+function principal() {
+    location.href = "principal.html"
+}
